476c8865f48652e8d32bd124197d3eb95a7b7f82
chantuff <chan.tuff@yahoo.com>
2024-01-13 13:03:26 -0500

    changed login page styling

--- src/pages/AuthPage/AuthPage.jsx
+++ src/pages/AuthPage/AuthPage.jsx
@@ -9,10 +9,20 @@ function AuthPage(props) {
   const [ showLogin, setShowLogin ] = useState(true);
 
   return (
-    <main className={styles.AuthPage} >
-            <h3 onClick={() => setShowLogin(!showLogin)}>{showLogin ? 'No account, SIGN UP here!' : 'LOG IN'}</h3>
-    { showLogin ? <LoginForm setUser={props.setUser} /> : <SignUpForm setUser={props.setUser}/>}
-    </ main>
+    // <main className={styles.AuthPage} >
+    //         {/* <h3 onClick={() => setShowLogin(!showLogin)}>{showLogin ? 'No account, SIGN UP here!' : 'LOG IN'}</h3>
+    // { showLogin ? <LoginForm setUser={props.setUser} /> : <SignUpForm setUser={props.setUser}/>} */}
+    <div className='container'>
+      <div className='box signin'>
+        <h2>Already have an Account?</h2>
+        <button className='signinBtn'>Sign In</button>
+      </div>
+      <div className='box signup'>
+        <h2>Don't have an Account?</h2>
+        <button className='signupBtn'>Sign Up</button>
+      </div>
+    </div>
+    // </ main>
   )
 }
 
