5eca47e170c9c3d6ec8a7e5cc1a4681f2499fb31
chantuff <chan.tuff@yahoo.com>
2024-01-13 13:15:11 -0500

    added container style and it works

--- src/pages/AuthPage/AuthPage.jsx
+++ src/pages/AuthPage/AuthPage.jsx
@@ -3,14 +3,15 @@ import styles from './AuthPage.module.css'
 import SignUpForm from '../../components/SignUpForm/SignUpForm'
 // import LoginForm from '../../components/LoginForm'
 import LoginForm from '../../components/LoginForm/LoginForm'
-// import Logo from '../../components/Logo/Logo'
+// import Logo from '../../components/Logo/Logo''
+
 
 function AuthPage(props) {
   const [ showLogin, setShowLogin ] = useState(true);
 
   return (
-    // <main className={styles.AuthPage} >
-    //         {/* <h3 onClick={() => setShowLogin(!showLogin)}>{showLogin ? 'No account, SIGN UP here!' : 'LOG IN'}</h3>
+    <main className={styles.AuthPage} >
+            {/* <h3 onClick={() => setShowLogin(!showLogin)}>{showLogin ? 'No account, SIGN UP here!' : 'LOG IN'}</h3>
     // { showLogin ? <LoginForm setUser={props.setUser} /> : <SignUpForm setUser={props.setUser}/>} */}
     <div className='container'>
       <div className='box signin'>
@@ -22,7 +23,7 @@ function AuthPage(props) {
         <button className='signupBtn'>Sign Up</button>
       </div>
     </div>
-    // </ main>
+     </ main>
   )
 }
 
